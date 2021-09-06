3cc1f98393f39e979b57a36bd08fcc7378cb8c92
Chris-Allen-AM <chrisa@amlabels.co.uk>
2021-09-06 14:31:16 +0100

    Add project files.

---
+++ MyCalc/Calc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalc
+{
+    public static class Calc
+    {
+        public decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+    }
+}
